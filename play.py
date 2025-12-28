50622619c3e3fc18cf6e3e7812416bbe4abfd2d1
Elliot Fackler <grant.fackler48@gmail.com>
2025-12-28 14:22:07 -0600

    Expanding round object to include player features.

--- play.py
+++ play.py
@@ -7,90 +7,93 @@ def play_game():
 
     deck2 = round.create_deck()
 
+    player1 = Player("Player 1")
+    player2 = Player("NPC")
 
-    while not round.is_player_turn_over or not round.is_npc_turn_over:
-        if not round.is_player_turn_over:
+
+    while not player1.is_turn_over or not player2.is_turn_over:
+        if not player1.is_turn_over:
             player_input = input("Would you like a new card? Y or N \n")
 
             # Check if the player wants a new card or if he/she would like to end his/her part in the round.
             if (player_input == "N" or player_input == "n"):
-                round.is_player_turn_over = True
+                player1.is_turn_over = True
 
                 # Count up player's points for the round.
-                round.player_score = calculate_score(round.player_number_cards_drawn, round.multiplier, round.player_score, 0)
+                player1.score = calculate_score(player1.number_cards_drawn, player1.multiplier, player1.score, 0)
 
             elif (player_input == "Y" or player_input == "y"):
-                chosen_card, deck2 = draw_a_card(deck2)
+                card, deck2 = draw_a_card(deck2)
                 
-                if (chosen_card in round.player_number_cards_drawn): # The player draws a card that he/she has already drawn
-                    print("You drew a card that you already have:", chosen_card)
-                    round.is_player_turn_over = True
+                if (card in player1.number_cards_drawn): # The player draws a card that he/she has already drawn
+                    print("You drew a card that you already have:", card)
+                    player1.is_turn_over = True
                     round.cards_drawn = []
-                    round.player_score = 0
-                elif (has_bonus_points(chosen_card)): # The player draws an additive point modifier card
-                    round.player_score = round.player_score + add_bonus_points(chosen_card)
-                elif (has_freeze(chosen_card)): # The player draws a freeze card
+                    player1.score = 0
+                elif (has_bonus_points(card)): # The player draws an additive point modifier card
+                    player1.score = player1.score + add_bonus_points(card)
+                elif (has_freeze(card)): # The player draws a freeze card
                     print("FREEZE!")
-                    round.is_npc_turn_over = True
-                elif (has_flip_three(chosen_card)):
+                    player2.is_turn_over = True
+                elif (has_flip_three(card)):
                     print("FLIP THREE!")
                     # TODO: Set up flip three
-                elif (has_second_chance(chosen_card)):
+                elif (has_second_chance(card)):
                     print("SECOND CHANCE!")
                     # TODO: Set up second chance.
-                elif (has_multiplier(chosen_card)):
-                    round.multiplier = 2
+                elif (has_multiplier(card)):
+                    player1.multiplier = 2
                 else: # The player draws a number card
-                    round.player_number_cards_drawn.append(chosen_card)
+                    player1.number_cards_drawn.append(card)
 
-                round.cards_drawn.append(chosen_card)
-                print("Your card is", chosen_card)
+                round.cards_drawn.append(card)
+                print("Your card is", card)
             else:
                 print("That input is not valid")
                 continue
 
-            if (has_flipped_seven(round.player_number_cards_drawn)): # Player has reached seven number cards. This means that the round is over and the player gets 15 extra points
+            if (has_flipped_seven(player1.number_cards_drawn)): # Player has reached seven number cards. This means that the round is over and the player gets 15 extra points
                 print("You've reached seven number cards. The round is over.")
-                round.is_player_turn_over = True
-                round.is_npc_turn_over = True
-                round.player_score = calculate_score(round.player_number_cards_drawn, round.multiplier, round.player_score, 15)
+                player1.is_turn_over = True
+                player2.is_turn_over = True
+                player1.score = calculate_score(player1.number_cards_drawn, player1.multiplier, player1.score, 15)
 
-        if not round.is_npc_turn_over:
-            npc_chosen_card, deck2 = draw_a_card(deck2)
+        if not player2.is_turn_over:
+            card, deck2 = draw_a_card(deck2)
 
-            if (npc_chosen_card in round.npc_number_cards_drawn): # The NPC draws a card that he/she has already drawn
-                print("The NPC drew a card that he already has:", npc_chosen_card)
-                round.is_npc_turn_over = True
+            if (card in player2.number_cards_drawn): # The NPC draws a card that he/she has already drawn
+                print("The NPC drew a card that he already has:", card)
+                player2.is_turn_over = True
                 round.npc_cards_drawn = []
-                round.npc_score = 0
-            elif (has_bonus_points(npc_chosen_card)): # The NPC draws an additive point modifier card
-                round.npc_score = round.npc_score + add_bonus_points(npc_chosen_card)
-            elif (has_freeze(npc_chosen_card)): # The NPC draws a freeze card
+                player2.score = 0
+            elif (has_bonus_points(card)): # The NPC draws an additive point modifier card
+                player2.score = player2.score + add_bonus_points(card)
+            elif (has_freeze(card)): # The NPC draws a freeze card
                 print("FREEZE!")
-                is_player_turn_over = True
-            elif (has_flip_three(npc_chosen_card)):
+                player1.is_turn_over = True
+            elif (has_flip_three(card)):
                 print("FLIP THREE!")
                 # TODO: Set up actual flip three stuff
-            elif (has_second_chance(npc_chosen_card)):
+            elif (has_second_chance(card)):
                 print("SECOND CHANCE!")
                 # TODO: Set up second chance.
-            elif (has_multiplier(npc_chosen_card)): # The NPC draws the multiplier
-                round.npc_multiplier = 2
+            elif (has_multiplier(card)): # The NPC draws the multiplier
+                player2.multiplier = 2
             else: # The NPC draws a number card
-                round.npc_number_cards_drawn.append(npc_chosen_card)
+                player2.number_cards_drawn.append(card)
 
-            round.npc_cards_drawn.append(npc_chosen_card)    
-            print("The NPC's card is", npc_chosen_card)
+            round.npc_cards_drawn.append(card)    
+            print("The NPC's card is", card)
 
-            if (has_flipped_seven(round.npc_number_cards_drawn)): # NPC has reached seven number cards. This means that the round is over and the NPC gets 15 extra points
-                round.is_player_turn_over = True
-                round.is_npc_turn_over = True
-                round.npc_score = calculate_score(round.npc_number_cards_drawn, round.npc_multiplier, round.npc_score, 15)
+            if (has_flipped_seven(player2.number_cards_drawn)): # NPC has reached seven number cards. This means that the round is over and the NPC gets 15 extra points
+                player1.is_turn_over = True
+                player2.is_turn_over = True
+                player2.score = calculate_score(player2.number_cards_drawn, player2.multiplier, player2.score, 15)
 
         print("The cards you have drawn so far this round are: ", round.cards_drawn, "\n")
         print("The cards the NPC has drawn so far this round are: ", round.npc_cards_drawn, "\n")
 
-    return round.player_score, round.npc_score
+    return player1.score, player2.score
 
 
 
